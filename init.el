f61656537ea3073291b041307e50ee304f52256f
gmsvalente <gustavomsvalente@gmail.com>
2023-01-02 21:07:53 -0300

    Update custom-file path

--- init.el
+++ init.el
@@ -15,11 +15,9 @@
 (message "---| Loading init.el |---")
 (message "-------------------------")
 
-;;; set custom-file.el
-(defvar custom-file-path (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file-path)
-  (setq custom-file custom-file-path)
-  (load custom-file))
+;;; set custom-file
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 ;;; disable on startup
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
@@ -78,15 +76,4 @@
 ;; 	(message (format "%s installed" pkg))
 ;;       (message (format "%s not installed" pkg)))))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
